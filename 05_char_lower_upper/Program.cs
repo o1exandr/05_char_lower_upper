bb2d380a27c8d30673d1301fd0ce58e8e5111896
olexandr.finyuk <olexandr.finyuk@gmail.com>
2018-09-16 18:24:49 +0300

    done

--- 05_char_lower_upper/Program.cs
+++ 05_char_lower_upper/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+5. Створити програму, яка вводить з клавіатури символи(посимвольно- Console.Read())
+ та перетворює малі букви у великі та навпаки.
+ */
+
+using System;
 
 namespace _05_char_lower_upper
 {
@@ -10,6 +11,21 @@ namespace _05_char_lower_upper
     {
         static void Main(string[] args)
         {
+            char c = ' ';
+            Console.Write("Enter some small and big letters, last symbol must be point '.'\n");
+            while (c != '.')
+            {
+                c = Convert.ToChar(Console.Read());
+                if (char.IsLower(c))
+                    Console.Write(Convert.ToChar(c - 32));
+                else
+                if (char.IsUpper(c))
+                    Console.Write(Convert.ToChar(c + 32));
+                else
+                    Console.Write(Convert.ToChar(c));
+            }
+
+            Console.WriteLine();
         }
     }
 }
